b574ab74d6b7dea4d97114bb03c9c7fef068b8f1
Stas <stas.shc@gmail.com>
2023-09-21 17:14:36 +0300

    crete weather

--- src/main/java/org/example/Weather.java
+++ src/main/java/org/example/Weather.java
@@ -1,4 +1,30 @@
 package org.example;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+@Getter
 public class Weather {
-    
+    //использовать патерн Observer для триггера id при изменении nameRegion
+    private int id;
+
+    private String nameRegion;
+    @Setter
+    private Double temperature;
+    @Setter
+    private ZonedDateTime creationDate;
+    public Weather(@NonNull String nameRegion,
+                   @NonNull Double temperature,
+                   @NonNull ZonedDateTime creationDate){
+        this.nameRegion = nameRegion;
+        this.id = this.nameRegion.hashCode();
+        this.temperature = temperature;
+        this.creationDate = creationDate;
+    }
+    public void setNameRegion(@NonNull String nameRegion) {
+        this.nameRegion = nameRegion;
+        this.id = nameRegion.hashCode();
+    }
 }
