cc3fee8348dbffe86f29b442d8b9b31c303448e6
Stas <stas.shc@gmail.com>
2023-09-23 20:14:56 +0300

    create class ReflectionConstants

--- src/main/java/org/example/FactoryWeather.java
+++ src/main/java/org/example/FactoryWeather.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import util.ReflectionConstants;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -24,7 +26,7 @@ public class FactoryWeather {
                                  ZonedDateTime creationData){
         try {
             Constructor<Weather> constructor = Weather.class.getDeclaredConstructor(
-                    UUID.class, String.class, Double.class, ZonedDateTime.class
+                    ReflectionConstants.CONSTRUCTOR_PARAMETERS
             );
             constructor.setAccessible(true);
             return constructor.newInstance(
@@ -37,12 +39,12 @@ public class FactoryWeather {
 
     }
     /**
-     * Setter имени региона (идентификатор также изменияется)
+     * Setter имени региона (идентификатор также изменяется)
      * */
     public void setWeatherNameRegion(Weather weather, String nameRegion){
         try {
-            Field uuidWeather = weather.getClass().getDeclaredField("uuid");
-            Field nameRegionWeather = Weather.class.getDeclaredField("nameRegion");
+            Field uuidWeather = weather.getClass().getDeclaredField(ReflectionConstants.UUID_FIELD);
+            Field nameRegionWeather = Weather.class.getDeclaredField(ReflectionConstants.NAME_REGION_FIELD);
             uuidWeather.setAccessible(true);
             nameRegionWeather.setAccessible(true);
             uuidWeather.set(weather, getUUIDFromNameRegion(nameRegion));
