93717dc200a02350ca8d7c3802dbc0ac3efc82cb
Stas <stas.shc@gmail.com>
2023-09-27 08:15:18 +0300

    package-private Weather constructor

--- src/main/java/weather/Weather.java
+++ src/main/java/weather/Weather.java
@@ -6,14 +6,13 @@ import java.util.UUID;
 
 /**
  * <p>Базовый класс</p>
- * <p>Конструктор приватный, чтобы было единственное место для создания объекта - FactoryWeather
- * (это уменьшает риск создания некорретного идентификатора). Создание объекта в фабрике реализовано
- * через Reflection API</p>
+ * <p>Конструктор package-private. Не стоит создавать объект класса напрямую через new
+ * лучше воспользоваться FactoryWeather</p>
  * <p>Setter для nameRegion аналогичным образом сделан в фабрике</p>
  * */
 @Getter
 @ToString
-@AllArgsConstructor(access=AccessLevel.PRIVATE)
+@AllArgsConstructor(access=AccessLevel.PACKAGE)
 public class Weather {
     /**
      * <p>Setter этого поля отсутсвует, т.к. идентификатор зависит от nameRegion</p>
@@ -21,8 +20,10 @@ public class Weather {
      * */
     private UUID uuid;
     /**
-     * Setter поля nameRegion реалиозован в FactoryWeather, который порадил этот объект
+     * <p>Setter поля nameRegion реалиозован в FactoryWeather, который порадил этот объект</p>
+     * <p>Не стоит использовать Setter напрямую из Weather, т.к. в нем не реализована смена uuid</p>
      * */
+    @Setter(value = AccessLevel.PACKAGE)
     private String nameRegion;
     @Setter
     private Double temperature;
