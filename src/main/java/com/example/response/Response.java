ff3ef04f56c191d4ffa6448c86940b9eed2204ef
Stas <stas.shc@gmail.com>
2023-10-01 22:41:42 +0300

    add ResponseGetTemperature

--- src/main/java/com/example/response/Response.java
+++ src/main/java/com/example/response/Response.java
@@ -1,11 +1,12 @@
-package com.example;
+package com.example.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
-@Setter@AllArgsConstructor
+@Setter
+@AllArgsConstructor
 public class Response {
+    private int codeStatus;
     private String message;
 }
