7b20d2e27c93fc07b88117dd43a8becb0cf72ca9
Stas <stas.shc@gmail.com>
2023-10-29 16:13:34 +0300

    create second test with spy

--- src/test/java/com/example/controllers/WeatherCityControllerTest.java
+++ src/test/java/com/example/controllers/WeatherCityControllerTest.java
@@ -2,24 +2,17 @@ package com.example.controllers;
 
 import com.example.WeatherApplication;
 import com.example.services.WeatherCityBehavior;
-import com.example.services.WeatherCityService;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Description;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
 
@@ -41,10 +34,13 @@ public class WeatherCityControllerTest {
     private static final String NAME_REGION = "Volgograd";
     private static final String DATE_STRING = "2022-01-01";
     private static final String NAME_DATE_PARAMETER = "localDate";
+    private final LocalDate DATE = LocalDate.parse(DATE_STRING);
     @InjectMocks
     private WeatherCityController weatherCityController;
     @MockBean
-    private WeatherCityBehavior weatherCityService;
+    private WeatherCityBehavior weatherCityServiceMock;
+    @SpyBean
+    private WeatherCityBehavior weatherCityServiceSpy;
 
     @Autowired
     private MockMvc mockMvc;
@@ -60,11 +56,10 @@ public class WeatherCityControllerTest {
      * */
     @Test
     public void getTemperatureByDate_validRegionAndDate_returnTemperature() throws Exception{
-        final LocalDate DATE = LocalDate.parse(DATE_STRING);
         final Double temperature = -10.;
-        when(weatherCityService.getTemperature(NAME_REGION, DATE))
+        when(weatherCityServiceMock.getTemperature(NAME_REGION, DATE))
                 .thenReturn(temperature);
-        when(weatherCityService.isEntryInBase(NAME_REGION, DATE))
+        when(weatherCityServiceMock.isEntryInBase(NAME_REGION, DATE))
                 .thenReturn(true);
 
         mockMvc.perform(get(URI, NAME_REGION)
@@ -72,7 +67,21 @@ public class WeatherCityControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.temperature").value(temperature));
-        verify(weatherCityService, times(1)).getTemperature(NAME_REGION, DATE);
+        verify(weatherCityServiceMock, times(1)).getTemperature(NAME_REGION, DATE);
+    }
+
+    @Test
+    public void getTemperatureByDate_invalidRegion_notFound() throws Exception{
+        final Double temperature = -10.;
+        when(weatherCityServiceSpy.getTemperature(NAME_REGION, DATE))
+                .thenReturn(temperature);
+        when(weatherCityServiceSpy.isEntryInBase(NAME_REGION, DATE))
+                .thenReturn(false);
+
+        mockMvc.perform(get(URI, NAME_REGION)
+                        .param(NAME_DATE_PARAMETER, DATE_STRING))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
 }
