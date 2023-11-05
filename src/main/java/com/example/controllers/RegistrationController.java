29d16c718c4e9d262bbbc598c9e4b5c53c4efba6
Stas <stas.shc@gmail.com>
2023-11-05 23:49:34 +0300

    add swagger

--- src/main/java/com/example/controllers/RegistrationController.java
+++ src/main/java/com/example/controllers/RegistrationController.java
@@ -3,6 +3,9 @@ package com.example.controllers;
 import com.example.entities.User;
 import com.example.response.Response;
 import com.example.services.RegistrationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +20,20 @@ import org.springframework.web.bind.annotation.*;
 public class RegistrationController {
 
     private final RegistrationService registrationService;
+
+    @Operation(
+            summary = "Метод регистрации"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Пользователь успешно добавлен в базу"
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Пользователь с таким username уже существует"
+            )
+    })
     @PostMapping
     public Response registration(@RequestBody User user){
         return registrationService.addNewUser(user);
